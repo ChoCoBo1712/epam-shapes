da828074bae3063cda0cd4780de807982758f438
ChoCoBo1712 <ChoCoBo1712@users.noreply.github.com>
2021-05-02 23:47:03 +0300

    rename

--- src/test/java/com/chocobo/shapes/reader/CubePointsReaderTest.java
+++ src/test/java/com/chocobo/shapes/reader/CubePointsReaderTest.java
@@ -7,7 +7,7 @@ import org.testng.annotations.Test;
 import java.util.Arrays;
 import java.util.List;
 
-public class CubePointsReadeTest {
+public class CubePointsReaderTest {
 
     private final CubePointsReader fileReader = new CubePointsReader();
 
