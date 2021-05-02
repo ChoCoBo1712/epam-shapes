40b279d2878ca6fea1912fe9babe56bd0ebe948a
ChoCoBo1712 <ChoCoBo1712@users.noreply.github.com>
2021-05-02 23:47:28 +0300

    parser test provider init

--- src/test/java/com/chocobo/shapes/parser/CubePointsParserTest.java
+++ src/test/java/com/chocobo/shapes/parser/CubePointsParserTest.java
@@ -1,15 +1,26 @@
 package com.chocobo.shapes.parser;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class CubePointsParserTest {
 
-    @Test
-    public void parseTest() {
+    @DataProvider(name = "string-provider")
+    public Object[][] stringProvider() {
+        return new Object[][] {
+                { "1.0, 2, 3.3, 4, 5", new double[] { 1.0, 2, 3.3, 4, 5 } },
+                { "1, +1, 2, 2", new double[] { 1, 1, 2, 2 } },
+                { "2.0, -5, 6, 4, 0.0, 0.0", new double[] { 2, -5, 6, 4, 0, 0 }  },
+                { "3, 4, 100.1, 5", new double[] { 3, 4, 100.1, 5 } },
+                { "1.3, 17, -1.554, 1.6", new double[] { 1.3, 17, -1.554, 1.6 } }
+        };
+    }
+
+    @Test(dataProvider = "string-provider")
+    public void parseTest(String line, double[] expected) {
         CubePointsParser parser = new CubePointsParser();
-        double[] expected = new double[]{1.0, 2, 3.3, 4, 5};
-        double[] actual = parser.parse("1.0, 2, 3.3, 4, 5");
+        double[] actual = parser.parse(line);
         Assert.assertEquals(actual, expected);
     }
 }
