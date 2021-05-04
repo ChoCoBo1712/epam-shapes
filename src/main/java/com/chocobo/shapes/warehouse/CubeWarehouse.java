bd40a126e1a48c6a7f7791eb31db4c196131a925
ChoCoBo1712 <ChoCoBo1712@users.noreply.github.com>
2021-05-04 15:59:57 +0300

    final fix

--- src/main/java/com/chocobo/shapes/warehouse/CubeWarehouse.java
+++ src/main/java/com/chocobo/shapes/warehouse/CubeWarehouse.java
@@ -1,12 +1,11 @@
 package com.chocobo.shapes.warehouse;
 
 import com.chocobo.shapes.entity.CubeParameter;
-import com.chocobo.shapes.exception.ShapeException;
 
 import java.util.Optional;
 
 public interface CubeWarehouse {
     Optional<CubeParameter> get(long id);
     void put(long id, double perimeter, double area, double volume);
-    void remove(Long id) throws ShapeException;
+    void remove(Long id);
 }
